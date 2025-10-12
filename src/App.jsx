d7ed43c34b497959245e636cc6b1fb3636993b27
AhmedAttia0 <AhmedAboulenein0147@gmail.com>
2025-10-13 02:42:58 +0300

    init

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import { Routes, Route } from "react-router";
+import Home from "./pages/home/Home";
+import Cars from "./pages/cars/Cars";
+import CarDetails from "./pages/carDetails/CarDetails";
+import BookingRequest from "./pages/bookingRequest/BookingRequest";
+import About from "./pages/about/About";
+import Contact from "./pages/contact/Contact";
+import Login from "./pages/login/Login";
+import SignUp from "./pages/signUp/SignUp";
+import NotFound from "./pages/notFound/NotFound";
+const App = () => {
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/cars" element={<Cars />} />
+        <Route path="/cars/:id" element={<CarDetails />} />
+        <Route path="/booking" element={<BookingRequest />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<SignUp />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </div>
+  );
+};
+
+export default App;
