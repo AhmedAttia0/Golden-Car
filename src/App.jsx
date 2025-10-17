ae22ce18357ba1af98be37c19f752d097451f65e
mostafa1115 <sasa123here@gmail.com>
2025-10-17 06:34:12 +0300

    use createBrowserRouter & create notFound page

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { Routes, Route } from "react-router";
+import { Routes, Route, Outlet, RouterProvider } from "react-router";
 import Home from "./pages/home/Home";
 import Cars from "./pages/cars/Cars";
 import Details from "./pages/carDetails/Details";
@@ -10,75 +10,27 @@ import SignUp from "./pages/signUp/SignUp";
 import NotFound from "./pages/notFound/NotFound";
 import Header from "./components/Header/Header";
 import Footer from "./components/footer/Footer";
+import { router } from "./router.jsx";
 const App = () => {
-  const carsList = [
-    {
-      id: 1,
-      price: 50,
-      name: "Toyota Camry",
-      Category: "Sedan",
-      Transmission: "اوتوماتيكي",
-      hasAirConditioner: true,
-    },
-    {
-      id: 2,
-      price: 20,
-      name: "Toyota Camry",
-      Category: "Sedan",
-      Transmission: "يدوي",
-      hasAirConditioner: true,
-    },
-    {
-      id: 3,
-      price: 100,
-      name: "Toyota Camry",
-      Category: "Sedan",
-      Transmission: "اوتوماتيكي",
-      hasAirConditioner: true,
-    },
-    {
-      id: 4,
-      price: 15,
-      name: "Toyota Camry",
-      Category: "Sedan",
-      Transmission: "اوتوماتيكي",
-      hasAirConditioner: false,
-    },
-    {
-      id: 5,
-      price: 20,
-      name: "Toyota Camry",
-      Category: "Sedan",
-      Transmission: "اوتوماتيكي",
-      hasAirConditioner: true,
-    },
-    {
-      id: 6,
-      price: 15,
-      name: "Toyota Camry",
-      Category: "Sedan",
-      Transmission: "اوتوماتيكي",
-      hasAirConditioner: false,
-    },
-  ];
   return (
-    <div className="min-h-screen flex flex-col">
-      <Header />
-      <main className="flex-1">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/cars" element={<Cars carsList={carsList} />} />
-          <Route path="/CarDetails" element={<Details />} />
-          <Route path="/booking" element={<BookingRequest />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<SignUp />} />
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </main>
-      <Footer />
-    </div>
+    <RouterProvider router={router} />
+    // <div className="min-h-screen flex flex-col">
+    //   <Header />
+    //   <main className="flex-1">
+    //     {/* <Routes>
+    //       <Route path="/" element={<Home />} />
+    //       <Route path="/cars" element={<Cars carsList={carsList} />} />
+    //       <Route path="/CarDetails" element={<Details />} />
+    //       <Route path="/booking" element={<BookingRequest />} />
+    //       <Route path="/about" element={<About />} />
+    //       <Route path="/contact" element={<Contact />} />
+    //       <Route path="/login" element={<Login />} />
+    //       <Route path="/register" element={<SignUp />} />
+    //       <Route path="*" element={<NotFound />} />
+    //     </Routes> */}
+    //   </main>
+    //   <Footer />
+    // </div>
   );
 };
 
