8003aab3c80b4f69fd9a2b3cacff138227f3a7b7
mostafa1115 <sasa123here@gmail.com>
2025-10-20 04:52:22 +0300

    fix a big

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,10 +1,12 @@
 import React from "react";
-import { Outlet } from "react-router";
+import { Outlet } from "react-router-dom";
+import ScrollToTop from "../components/ScrollToTop/ScrollToTop";
 import Header from "../components/Header/Header";
 import Footer from "../components/footer/Footer";
 export default function MainLayout() {
   return (
     <div className="min-h-screen flex flex-col">
+      <ScrollToTop behavior="smooth" />
       <Header />
       <main className="flex-1">
         <Outlet />
--- src/main.jsx
+++ src/main.jsx
@@ -2,8 +2,6 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import { ThemeProvider } from "@material-tailwind/react";
-import { RouterProvider } from "react-router";
-import { router } from "./router.jsx";
 createRoot(document.getElementById("root")).render(
   <ThemeProvider>
     <App />
