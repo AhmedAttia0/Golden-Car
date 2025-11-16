11d8bbdb8482ae9b8825912d349ca8063e80b806
mostafa1115 <sasa123here@gmail.com>
2025-11-16 18:38:34 +0200

    Make header response to clicks & make pagination component & highlight the current selected car

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -2,13 +2,16 @@ import { Outlet } from "react-router-dom";
 import ScrollToTop from "../components/ScrollToTop/ScrollToTop";
 import Header from "../components/Header/Header";
 import Footer from "../components/footer/Footer";
+import { CarsProvider } from "../context/CarsContext";
 export default function MainLayout() {
   return (
     <div className="min-h-screen flex flex-col">
       <ScrollToTop behavior="smooth" />
       <Header />
       <main className="flex-1">
-        <Outlet />
+        <CarsProvider>
+          <Outlet />
+        </CarsProvider>
       </main>
       <Footer />
     </div>
