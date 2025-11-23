b9a5c21683a70ecc67c62c00816fbebfa069502b
mostafa1115 <sasa123here@gmail.com>
2025-11-23 14:57:33 +0200

    filter component

--- src/hooks/useInfiniteCar.jsx
+++ src/hooks/useInfiniteCar.jsx
@@ -1,17 +1,12 @@
 import { getCars } from "../Services/dataService";
 import { useInfiniteQuery } from "@tanstack/react-query";
-export default function useInfiniteCar(filters, initialData) {
+export default function useInfiniteCar(filters) {
   return useInfiniteQuery({
     queryKey: ["cars", filters],
     queryFn: async ({ pageParam = 1 }) => getCars({ pageParam }, filters),
     getNextPageParam: (lastPage) => {
       if (!lastPage.hasMore) return undefined;
       return lastPage.page + 1;
-    },
-
-    initialData: {
-      pages: [initialData],
-      pageParams: [1],
     },
 
     keepPreviousData: true,
