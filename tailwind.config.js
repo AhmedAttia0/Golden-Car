10a7fb5d2d58d8ad9b71d7207d87af1876cbb3c2
Abdallah Mahmoud <abdallahmahmoud5590@gmail.com>
2025-11-24 18:38:50 +0200

    settings page in dashboard

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,21 +1,18 @@
 const withMT = require("@material-tailwind/react/utils/withMT");
+
 /** @type {import('tailwindcss').Config} */
 module.exports = withMT({
   content: [
-    "./index.html",
-    "./src/**/*.{js,jsx}",
-    "./node_modules/@material-tailwind/react/components/**/*.{js,ts,jsx,tsx}",
-    "./node_modules/@material-tailwind/react/theme/components/**/*.{js,ts,jsx,tsx}",
+    "./src/**/*.{js,ts,jsx,tsx}", 
   ],
   theme: {
     extend: {
-       colors: {
-        div: {
-          lightgray: "#FAFAFA",
-          purple:"#5937E0"
+      colors: {
+        lightgray: "#FAFAFA",
+        purple: "#5937E0",
+        navyblue: "#24272F",
+      },
     },
-  },
-},
   },
   plugins: [],
 });
