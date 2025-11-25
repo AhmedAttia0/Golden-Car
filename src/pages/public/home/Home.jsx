4d721b4572af7049a24fbd93c56b4542d991f760
AhmedAttia0 <AhmedAboulenein0147@gmail.com>
2025-11-25 06:18:48 +0200

    home second

--- src/pages/public/home/Home.jsx
+++ src/pages/public/home/Home.jsx
@@ -1,20 +1,18 @@
-
-import HeroSection from './../../../components/heroSection/HeroSection';
-import InfoHome from './../../../components/infoHome/InfoHome';
-import SecondInfoHome from './../../../components/secondInfoHome/SecondInfoHome';
-import CarsHome from './../../../components/carsHome/CarsHome';
-import NombersHome from './../../../components/nombersHome/NombersHome';
+import HeroSection from "./../../../components/heroSection/HeroSection";
+import InfoHome from "./../../../components/infoHome/InfoHome";
+import SecondInfoHome from "./../../../components/secondInfoHome/SecondInfoHome";
+import CarsHome from "./../../../components/carsHome/CarsHome";
+import NombersHome from "./../../../components/nombersHome/NombersHome";
 
 const Home = () => {
   return (
-    <div className="container mx-auto">
+    <div className="lg:container lg:mx-auto mx-3">
       <HeroSection />
       <div className="flex flex-col gap-14">
         <InfoHome />
         <SecondInfoHome />
         <CarsHome />
         <NombersHome />
-        
       </div>
     </div>
   );
