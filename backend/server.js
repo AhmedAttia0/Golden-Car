b4a0376bff8bf6c58c1f896645f03b3d9b81b1dc
mostafa1115 <sasa123here@gmail.com>
2025-12-08 20:33:42 +0200

    booking backend

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,10 @@ import express from "express";
 import { connect } from "mongoose";
 import carRotuer from "./src/routes/cars.mjs";
 import settingsRouter from "./src/routes/settings.mjs";
+import bookingRouter from "./src/routes/booking.mjs";
+import path from "path";
+const port = 3000;
+
 connect(
   "mongodb+srv://mostafamozakiii_db_user:i9hpIDVe5uSEzaPg@cluster0.tcmujto.mongodb.net/Golden_Car?appName=Golden_Car&retryWrites=true&w=majority"
 )
@@ -9,8 +13,11 @@ connect(
   .catch((err) => console.error("Could not connect to MongoDB", err));
 
 const app = express();
+
 app.use(express.json());
+app.use("/uploads", express.static(path.join(process.cwd(), "uploads")));
 app.use("/cars", carRotuer);
 app.use("/settings", settingsRouter);
+app.use("/booking", bookingRouter);
 
-app.listen(3000, () => console.log("Server listening on port 3000"));
+app.listen(port, () => console.log("Server listening on port ", port));
--- frontend/src/pages/admin/CarsManagement/Filterations.jsx
+++ frontend/src/pages/admin/CarsManagement/Filterations.jsx
@@ -1,8 +1,8 @@
 import { useState } from "react";
 import { IoIosSearch } from "react-icons/io";
-import { IoIosArrowDown } from "react-icons/io";
 import CreateNewCarModal from "./CreateOrEditCarModal";
 import { useSearchParams } from "react-router-dom";
+import Dropdown from "../../../components/Dropdown";
 const statusOptions = ["كل السيارات", "متاحة", "غير متاحة", "تحت الصيانة"];
 const carTypeOptions = ["suv", "sedan", "cabriolet", "pickup", "minivan"];
 
@@ -26,6 +26,15 @@ export default function Filterations() {
     setSearchParams(params);
   };
 
+  function handleDropdownChange(option, menuKey) {
+    const params = Object.fromEntries(searchParams.entries());
+    delete params[menuKey];
+    if (option !== "كل السيارات") {
+      params[menuKey] = option;
+    }
+    setSearchParams(params);
+  }
+
   return (
     <div className="flex gap-4 flex-wrap w-full overflow-visible">
       <CreateNewCarModal />
@@ -40,60 +49,17 @@ export default function Filterations() {
         <IoIosSearch className="absolute left-3 top-1/2 -translate-y-1/2 text-gray-500 h-5 w-5" />
       </div>
       <div className="w-[98%] md:w-[48%] lg:w-[200px] relative">
-        <CarDropdown menuKey={"status"} options={statusOptions} />
+        <Dropdown
+          onChange={(option) => handleDropdownChange(option, "status")}
+          options={statusOptions}
+        />
       </div>
       <div className="w-[98%] md:w-[48%] lg:w-[200px] relative">
-        <CarDropdown menuKey={"type"} options={carTypeOptions} />
+        <Dropdown
+          onChange={(option) => handleDropdownChange(option, "type")}
+          options={carTypeOptions}
+        />
       </div>
     </div>
-  );
-}
-
-export function CarDropdown({ options, menuKey, borderColor = "#4e4e4e" }) {
-  const [open, setOpen] = useState(false);
-  const [selected, setSelected] = useState(options[0]);
-  const [searchParams, setSearchParams] = useSearchParams();
-  const toggleDropdown = () => setOpen(!open);
-  const selectOption = (option) => {
-    setSelected(option);
-    setOpen(false);
-    const params = Object.fromEntries(searchParams.entries());
-    delete params[menuKey];
-    if (option !== "كل السيارات") {
-      params[menuKey] = option;
-    }
-    setSearchParams(params);
-  };
-
-  return (
-    <>
-      {/* Button */}
-      <button
-        onClick={toggleDropdown}
-        className={`w-full border border-[${borderColor}] rounded-lg px-3 py-2 flex justify-between items-center bg-transparent text-right focus:outline-none text-white`}
-      >
-        {selected}
-        <IoIosArrowDown
-          className={`h-5 w-5 transition-transform duration-200 ${
-            open ? "rotate-180" : "rotate-0"
-          }`}
-        />
-      </button>
-
-      {/* Options */}
-      {open && (
-        <ul className="absolute mt-1 w-full bg-[#14151a] text-white font-bold border-gray-300 rounded-md shadow-lg z-50">
-          {options.map((option) => (
-            <li
-              key={option}
-              onClick={() => selectOption(option)}
-              className="px-3 py-2 hover:bg-blue-500 hover:text-white cursor-pointer"
-            >
-              {option}
-            </li>
-          ))}
-        </ul>
-      )}
-    </>
   );
 }
