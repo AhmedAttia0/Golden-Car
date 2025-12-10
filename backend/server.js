5d9cee34ee457f75f3a63e394b1b47f039f84ac6
AhmedAttia0 <167963166+AhmedAttia0@users.noreply.github.com>
2025-12-10 03:07:08 +0200

    Refactor server.js to remove user routes and use env vars
    Removed user-related routes and middleware, and updated MongoDB connection to use environment variables.

--- backend/server.js
+++ backend/server.js
@@ -3,43 +3,22 @@ import { connect } from "mongoose";
 import carRotuer from "./src/routes/cars.mjs";
 import settingsRouter from "./src/routes/settings.mjs";
 import bookingRouter from "./src/routes/booking.mjs";
-import User from "./src/models/User.mjs";
-import validateToken from "./src/middleware/auth";
-import userRouter from "./src/routes/user.mjs";
-import cookieSession from "cookie-session";
 import path from "path";
-const port = 3000;
+import dotenv from "dotenv";
+dotenv.config();
 
-connect(process.env.DB_LINK)
+const uri = process.env.MONGO_URI;
+const port = process.env.PORT || 5000;
+
+connect(uri)
   .then(() => console.log("Connected to MongoDB"))
   .catch((err) => console.error("Could not connect to MongoDB", err));
 
 const app = express();
 
 app.use(express.json());
-app.use(
-  cookieSession({
-    name: "session",
-    keys: [process.env.COOKIE_SECRET],
-    maxAge: 2 * 60 * 60 * 1000,
-  })
-);
 app.use("/uploads", express.static(path.join(process.cwd(), "uploads")));
 app.use("/cars", carRotuer);
-app.use("/users", userRouter);
-app.get("/", validateToken, async (req, res) => {
-  try {
-    const userDoc = await User.findById(req.id).lean(); // plain JS object
-    if (!userDoc) {
-      return res.status(404).json({ message: "User not found" });
-    }
-    const { password, role, _id, ...rest } = userDoc;
-    const sanitizedUser = { id: _id, ...rest };
-    return res.status(200).json({ user: sanitizedUser });
-  } catch (err) {
-    return res.status(500).json({ message: "Server error" });
-  }
-});
 app.use("/settings", settingsRouter);
 app.use("/booking", bookingRouter);
 
